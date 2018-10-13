7b98e03a553b71b4f32cc0c28d23559217562669
unknown <kevers429@gmail.com>
2018-10-13 04:08:37 -0700

    parse app info

--- Projects/P2/Code/defn.h
+++ Projects/P2/Code/defn.h
@@ -10,12 +10,6 @@ struct categories{
 	struct tree *root;  // Pointer to root of search tree for this category
 };
 
-struct tree{ // A binary search tree
-	struct app_info info; // Information about the application
-	struct tree *left;  // Pointer to the left subtree
-	struct tree *right;  // Pointer to the right subtree
-};
-
 struct app_info{
 	char category[ CAT_NAME_LEN ]; // Name of category
 	char app_name[ APP_NAME_LEN ]; // Name of the application
@@ -23,6 +17,12 @@ struct app_info{
 	float size; // Size of the application
 	char units[ UNIT_SIZE ]; // GB or MB
 	float price; // Price in $ of the application
+};
+
+struct tree{ // A binary search tree
+	struct app_info info; // Information about the application
+	struct tree *left;  // Pointer to the left subtree
+	struct tree *right;  // Pointer to the right subtree
 };
 
 struct hash_table_entry{
--- Projects/P2/Code/main.cpp
+++ Projects/P2/Code/main.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
+#include <string.h>
 #include "defn.h"
 
 using namespace std;
 
+categories* appList;
+float size, price;
+int numCategories, numApps;
+char category[CAT_NAME_LEN], app_name[APP_NAME_LEN], version[VERSION_LEN], units[UNIT_SIZE];
+
 int main() {
-  
+  cin >> numCategories;
+  cin.ignore(1,'\n');
+  appList = new categories[numCategories];
+  for(int i = 0; i < numCategories; i++) {
+    cin.getline(appList[i].category, CAT_NAME_LEN);
+    appList[i].root = new tree;
+  }
+  cin >> numApps;
+  for(int i = 0; i < numApps; i++) {
+    cin.ignore(1,'\n');
+    cin.getline(category, CAT_NAME_LEN);
+    cin.getline(app_name, APP_NAME_LEN);
+    cin >> version >> size >> units >> price;
+    app_info tmpApp;
+    strcpy(tmpApp.category, category);
+    strcpy(tmpApp.app_name, app_name);
+    strcpy(tmpApp.version, version);
+    strcpy(tmpApp.units, units);
+    tmpApp.size = size;
+    tmpApp.price = price;
+  }
   return 0;
 }
